a5f9f4665fa24ff5a68cf21a5d799a219c0a79b7
Nithin <tamnurunithin@gmail.com>
2025-08-20 19:32:44 +0530

    Insertion Sort Notes & Example

---
+++ Insertion Sort/InsertionSort.java
@@ -0,0 +1,23 @@
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];   
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+        insertionSort(arr);
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
